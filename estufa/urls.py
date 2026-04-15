d2c71eb3b64e5117fd82101839153d93f3273117
Rair Gabriel <rairgabrielrgl@gmail.com>
2026-04-15 16:26:24 -0300

    Atual

--- estufa/urls.py
+++ estufa/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
+
+
 
 urlpatterns = [
+    path('', home),  # 👈 ADICIONA ISSO
     path('admin/', admin.site.urls),
     path('monitoramento/', include('monitoramento.urls')),
 ]
