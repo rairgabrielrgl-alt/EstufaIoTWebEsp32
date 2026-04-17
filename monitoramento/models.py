94e956755075c1e764e71bd458b40e91dba02f79
Rair Gabriel <rairgabrielrgl@gmail.com>
2026-04-17 10:01:56 -0300

    atualização status

--- monitoramento/models.py
+++ monitoramento/models.py
@@ -3,7 +3,12 @@ from django.db import models
 class LeituraSensor(models.Model):
     temperatura = models.FloatField()
     umidade = models.FloatField()
+    ventoinha = models.BooleanField(default=False)
+    umidificador = models.BooleanField(default=False)
     data = models.DateTimeField(auto_now_add=True)
 
 class Controle(models.Model):
     ventoinha = models.BooleanField(default=False)    
+
+
+
