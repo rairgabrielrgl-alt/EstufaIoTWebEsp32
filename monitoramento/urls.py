d7b8dc9e308105ff021019a237eab61c3b060c65
Rair Gabriel <rairgabrielrgl@gmail.com>
2026-05-25 21:42:15 -0300

    atualização status12

--- monitoramento/urls.py
+++ monitoramento/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('dados/', views.receber_dados),
-    path('painel/', views.painel, name='painel'),
+    path('painel/', views.painel),
     path('api/', views.api_dados),
     path('controle/', views.controle),
     path('estado/', views.estado),
